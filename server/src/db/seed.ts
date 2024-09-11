acc71c863c9c17a773378da1f1e71cedb6a1a102
JhowSL <jon.1lima@hotmail.com>
2024-09-11 13:36:40 -0300

    Refactor code formatting and fix linting issues

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,22 +1,25 @@
-import dayjs from "dayjs"
-import { client, db } from "."
-import { goalCompletions, goals } from "./schema"
+import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
-  const result = await db.insert(goals).values([
-    { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-    { title: 'Estudar inglês', desiredWeeklyFrequency: 3 },
-    { title: 'Fazer exercícios', desiredWeeklyFrequency: 4 },
-  ]).returning()
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { title: 'Estudar inglês', desiredWeeklyFrequency: 3 },
+      { title: 'Fazer exercícios', desiredWeeklyFrequency: 4 },
+    ])
+    .returning()
 
   const startOfWeek = dayjs().startOf('week')
 
   await db.insert(goalCompletions).values([
     { goalId: result[0].id, completedAt: startOfWeek.toDate() },
-    { goalId: result[1].id, completedAt: startOfWeek.add(1, 'day').toDate() }
+    { goalId: result[1].id, completedAt: startOfWeek.add(1, 'day').toDate() },
   ])
 }
 
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,32 +1,37 @@
-import fastify from "fastify";
-import { createGoal } from "../functions/create-goal";
-import z from "zod";
+import fastify from 'fastify'
 import {
+  type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
-  ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
 
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.post('/goals', {
-  schema: {
-    body: z.object({
-      title: z.string(),
-      desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    }),
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
   }
-}, async request => {
-  const { title, desiredWeeklyFrequency } = request.body
+)
 
-  await createGoal({
-    title,
-    desiredWeeklyFrequency,
-  })
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP Server is running 🚀')
 })
-
-app.listen({ port: 3333 }).then(() => { console.log('HTTP Server is running 🚀') });
